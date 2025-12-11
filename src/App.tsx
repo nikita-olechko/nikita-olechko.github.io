3d70e33a4604974b799b6d4588ab2eb35bb3d84f
nikita-olechko <nikita.olechko@gmail.com>
2025-12-10 20:53:32 -0800

    Update wording

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function App(): JSX.Element {
   return (
     <div style={{ padding: '2rem', maxWidth: '800px', margin: '0 auto' }}>
-      <h1>Hello, website belongs to Nikita Olechko.</h1>
+      <h1>Hello, this website belongs to Nikita Olechko.</h1>
       <p>You can contact me at <a href="mailto:nikita.olechko@gmail.com">nikita.olechko@gmail.com</a></p>
       <p>Pretend there's more things here.</p>
     </div>
